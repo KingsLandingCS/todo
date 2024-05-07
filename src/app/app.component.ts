2cddba75013a1cd084da332cfaa21b4c73f039e5
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-05-07 11:20:18 +0500

    learning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,6 +25,13 @@ export class AppComponent {
   }
 
   get items(): readonly TodoItem[] {
-    return this.list.items;
+    return this.list.items.filter(item => !item.complete);
   }
+
+  addItem(newItem: string) {
+    if (newItem != "") {
+      this.list.addItem(newItem);
+    }
+  }
+  showComplete: boolean = false;
 }
